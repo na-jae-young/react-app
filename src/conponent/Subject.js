787df06a7c8077c7c527b0a2030a4f7ed03c12cc
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-27 17:47:21 +0900

    e.preventDefault(); 기본적으로 동작되던 기능을 막아줌

--- src/conponent/Subject.js
+++ src/conponent/Subject.js
@@ -5,7 +5,12 @@ class Subject extends Component{
     render(){
       return(
         <header>
-          <h1><a href="/">{this.props.title}</a></h1>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            alert('hi')
+            e.preventDefault(); // reload 안되게 막아줌 
+            //기본적으로 작동하던것들을 작동하지 않게 막아주는 함수 
+          }}>{this.props.title}</a></h1>
           {this.props.sub}
         </header>
       )
