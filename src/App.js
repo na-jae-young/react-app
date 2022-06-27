3a5dc3b46b6a347fd1857f9c8dd3dfd694ad4633
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-27 15:08:41 +0900

    Component 모듈화  App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component}from 'react';
+import './App.css';
+import Content from './conponent/Content';
+import Toc from './conponent/Toc';
+import Subject from './conponent/Subject';
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        HELLO WORLD
+        <Subject title="WEB" sub="world wide web!" ></Subject>
+        <Toc></Toc>
+        <Content title="html" desc="html is ....."></Content>
+      </div>
+    );
+  }
 }
+
 
 export default App;
