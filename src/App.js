b9ac17c305042a33641505f16dfdae6b0f67f3d6
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-27 16:16:22 +0900

    props, state 초기화

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import Subject from './conponent/Subject';
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:'web', sub:'world wide web!'}
+    } //state 초기화
+
+  }
   render(){
     return(
       <div className="App">
         HELLO WORLD
-        <Subject title="WEB" sub="world wide web!" ></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub} >
+        </Subject>
         <Toc></Toc>
         <Content title="html" desc="html is ....."></Content>
       </div>
