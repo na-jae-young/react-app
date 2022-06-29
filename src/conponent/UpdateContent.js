f09eb7343b8bab1e523427693a0e75f1d6c46e43
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-29 10:54:54 +0900

    onChange  생성 간단하게   핸들러 생성

--- src/conponent/UpdateContent.js
+++ src/conponent/UpdateContent.js
@@ -7,7 +7,13 @@ class UpdateContent  extends Component{
             title:this.props.data.title,
             desc:this.props.data.desc
         } // props는 readonly이기 때문에 수정이 불가하므로 conponent내부에 state화 시켜준후 사용한다.
+        this.inputFormHandler = this.inputFormHandler.bind(this)
     }
+
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+
     render(){
         const read_content = this.props.data //이런식으로 갖어와서 사용하면 readonly 
       return(
@@ -28,9 +34,7 @@ class UpdateContent  extends Component{
                         name="title" 
                         placeholder='title' 
                         value={this.state.title}
-                        onChange={function(e){
-                            this.setState({title:e.target.value})
-                        }.bind(this)}>
+                        onChange={this.inputFormHandler}>
                     </input>
                 </p>
                 <p>
@@ -39,9 +43,7 @@ class UpdateContent  extends Component{
                         name="desc" 
                         placeholder='description' 
                         value={this.state.desc}
-                        onChange={function(e){
-                            this.setState({desc:e.target.value})
-                        }.bind(this)}> 
+                        onChange={this.inputFormHandler}> 
                     </input>
                 </p>
                 <p><input type="submit" ></input></p>
